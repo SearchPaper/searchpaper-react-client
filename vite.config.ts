0d86426407f1b154c52c8cd4313f93fab5addb96
Lucas Pereira dos Santos <lucasdev2001@gmail.com>
2025-04-24 16:01:19 -0300

    Add: proxy pass to api for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import "dotenv/config"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": process.env.API_ADDRESS ?? "http://localhost:5003"
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
