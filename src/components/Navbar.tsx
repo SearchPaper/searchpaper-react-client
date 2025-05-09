a4b1343e932e1e6ae6a3e828be2285719d414fb6
Lucas Pereira dos Santos <lucasdev2001@gmail.com>
2025-05-09 19:34:32 -0300

    Refactor: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,13 @@ import { Link } from "wouter";
 
 export function NavbarDropdownMenu() {
   return (
-    <div className="dropdown dropdown-end ">
-      <div tabIndex={0} role="button" className="btn btn-square btn-neutral">
-        <i className="fa-solid fa-ellipsis" />
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <i className="fa-solid fa-bars"></i>
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 z-1 mt-3 w-52 shadow-xl gap-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
       ></ul>
     </div>
   );
@@ -17,13 +17,13 @@ export function NavbarDropdownMenu() {
 export function Navbar() {
   return (
     <>
-      <nav className="navbar bg-base-100">
+      <nav className="navbar">
         <div className="navbar-start">
-          <Link to="/" className="btn btn-primary ">
+          <Link to="/" className="btn btn-primary font-normal">
             SearchPaper
           </Link>
         </div>
-
+        <div className="navbar-center"></div>
         <div className="navbar-end">
           <NavbarDropdownMenu />
         </div>
