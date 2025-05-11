68bed452165064d59ac0f3bf2b23515f6e42e621
Lucas Pereira dos Santos <lucasdev2001@gmail.com>
2025-05-11 11:36:53 -0300

    Refactor: Response Alerts

--- src/components/ResponseAlerts.tsx
+++ src/components/ResponseAlerts.tsx
@@ -1,33 +1,30 @@
 import { Alert, useAlertStore } from "../stores/alertStore";
 
 function Alerts({ alerts }: { alerts: Alert[] }) {
-  return (
-    <>
-      {alerts.map((alert) => {
-        const alertSuccess = "alert-success";
-        const alertError = "alert-error";
-        const { response, message } = alert;
+  const alertRow = alerts.map((alert) => {
+    const alertSuccess = "alert-success";
+    const alertError = "alert-error";
+    const { response, message } = alert;
 
-        const { status, ok } = response;
+    const { ok } = response;
 
-        return (
-          <div
-            className={`alert ${ok ? alertSuccess : alertError}`}
-            key={alert._id}
-          >
-            <span>{status}</span>
-            <span>{message}</span>
-          </div>
-        );
-      })}
-    </>
-  );
+    return (
+      <div
+        className={`alert  ${ok ? alertSuccess : alertError}`}
+        key={alert._id}
+      >
+        <span>{message}</span>
+      </div>
+    );
+  });
+
+  return alertRow;
 }
 
 export default function ResponseAlerts() {
   const alerts = useAlertStore((state) => state.alerts);
   return (
-    <div className="toast toast-top toast-center">
+    <div className="toast toast-top toast-center z-50">
       <Alerts alerts={alerts} />
     </div>
   );
